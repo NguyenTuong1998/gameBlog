b94e9ecdef918cdff7580316bb3136c02261afe6
NguyenTuong <nhattuong.nguyen@kingfoodmart.net>
2023-07-09 17:58:56 +0700

    add gg ana

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import TwSizeIndicator from "@components/TwSizeIndicator";
 import config from "@config/config.json";
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 const Document = () => {
   // destructuring items from config object
@@ -24,6 +25,21 @@ const Document = () => {
           content="#000"
         />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8948874222142276" crossorigin="anonymous"></script>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        />
+
+        <Script id="ga-script" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+        </Script>
       </Head>
       <body>
         <Main />
